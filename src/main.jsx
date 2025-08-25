ae64f914bff3eaede5288e56e2209013c4454bde
Arfin Usmaan <arfinusmaan008@gmail.com>
2025-08-25 14:54:33 +0530

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
+import { StrictMode, lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+const App = lazy(() => import('./App.jsx'));
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
   </StrictMode>
 );
